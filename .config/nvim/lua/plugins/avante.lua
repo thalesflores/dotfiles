c45ae49e3909ec3ddc98ea0b9f46df08d4bc2a66
Thales Flores <22772984+thalesflores@users.noreply.github.com>
2025-06-19 16:19:57 -0400

    Add treesj and avante

---
+++ .config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,23 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false,
+  opts = {},
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "ibhagwan/fzf-lua",
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "Avante" },
+      },
+      ft = { "Avante" },
+    },
+  },
+}
