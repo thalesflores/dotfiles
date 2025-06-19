74e95370748980f736ae5998e14d25641ab22b83
Thales Flores <22772984+thalesflores@users.noreply.github.com>
2025-06-19 16:04:38 -0400

    Update vim-tmux-navigator config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,18 +1,32 @@
 return {
   {
-  'shaunsingh/nord.nvim', lazy = false, priority = 1000 },   -- colorscheme
-  'tpope/vim-surround',                                      -- surrounding
-  'vim-scripts/tComment',                                    -- Add shortcuts for commenting block of codes.
-  'jiangmiao/auto-pairs',                                    -- closing braces and brackets
-  'tpope/vim-obsession',                                     -- Restore session along with tmux-ressurect
-  'machakann/vim-highlightedyank',                           -- highlight when use y
-  'bogado/file-line',                                        -- in to enable vim to open the file in the spefic line
-  'eugen0329/vim-esearch',                                   -- search
-  'benmills/vimux',                                          -- Creates or run commands from VIM on existing TMUX panes.
-  'christoomey/vim-tmux-navigator',                          -- tmux integration
-  'RRethy/nvim-treesitter-endwise',                          -- auto add end in functions
-  'mhinz/vim-startify',                                      -- fancy initial screen to vim
-  'tpope/vim-eunuch',                                        -- Easialy rename files using :Rename filename newfilename
+    'shaunsingh/nord.nvim', lazy = false, priority = 1000 }, -- colorscheme
+  'tpope/vim-surround',                                    -- surrounding
+  'vim-scripts/tComment',                                  -- Add shortcuts for commenting block of codes.
+  'jiangmiao/auto-pairs',                                  -- closing braces and brackets
+  'tpope/vim-obsession',                                   -- Restore session along with tmux-ressurect
+  'machakann/vim-highlightedyank',                         -- highlight when use y
+  'bogado/file-line',                                      -- in to enable vim to open the file in the spefic line
+  'eugen0329/vim-esearch',                                 -- search
+  'benmills/vimux',                                        -- Creates or run commands from VIM on existing TMUX panes.
+  {
+    'christoomey/vim-tmux-navigator',
+
+    init = function()
+      vim.g.tmux_navigator_no_mappings = true
+      vim.g.tmux_navigator_no_wrap = true
+    end,
+
+    config = function()
+      vim.keymap.set('n', '<C-h>', ':<C-U>TmuxNavigateLeft<CR>', { silent = true })
+      vim.keymap.set('n', '<C-j>', ':<C-U>TmuxNavigateDown<CR>', { silent = true })
+      vim.keymap.set('n', '<C-k>', ':<C-U>TmuxNavigateUp<CR>', { silent = true })
+      vim.keymap.set('n', '<C-l>', ':<C-U>TmuxNavigateRight<CR>', { silent = true })
+    end,
+  },                                                     -- tmux integration
+  'RRethy/nvim-treesitter-endwise',                      -- auto add end in functions
+  'mhinz/vim-startify',                                  -- fancy initial screen to vim
+  'tpope/vim-eunuch',                                    -- Easialy rename files using :Rename filename newfilename
 
   -- Todo highligth and list
   {
