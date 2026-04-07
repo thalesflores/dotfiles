8f419dc948b32b64a97eb4f8940a3d359ea58eb6
thalesflores <22772984+thalesflores@users.noreply.github.com>
2026-04-07 05:08:57 -0400

    Drop nvim-lspconfig plugin and disable postgres_lsp
    Neovim 0.12 ships built-in LSP configs for all servers, making the
    nvim-lspconfig plugin redundant. mason-lspconfig now calls vim.lsp.enable()
    directly, so the setup flow is: Mason installs binaries → mason-lspconfig
    auto-enables them → vim.lsp.config() overrides apply.
    
    Also disable postgres_lsp (binary not available) and reformat indentation
    from 4-space tabs to 2-space consistency.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,52 +1,63 @@
 return {
-  {
-  'shaunsingh/nord.nvim', lazy = false, priority = 1000 },   -- colorscheme
-  'tpope/vim-surround',                                      -- surrounding
-  'vim-scripts/tComment',                                    -- Add shortcuts for commenting block of codes.
-  'jiangmiao/auto-pairs',                                    -- closing braces and brackets
-  'machakann/vim-highlightedyank',                           -- highlight when use y
-  'bogado/file-line',                                        -- in to enable vim to open the file in the spefic line
-  'eugen0329/vim-esearch',                                   -- search
-  'RRethy/nvim-treesitter-endwise',                          -- auto add end in functions
-  'mhinz/vim-startify',                                      -- fancy initial screen to vim
-  'tpope/vim-eunuch',                                        -- Easialy rename files using :Rename filename newfilename
+	{
+		"shaunsingh/nord.nvim",
+		lazy = false,
+		priority = 1000,
+	}, -- colorscheme
+	"tpope/vim-surround", -- surrounding
+	"vim-scripts/tComment", -- Add shortcuts for commenting block of codes.
+	"jiangmiao/auto-pairs", -- closing braces and brackets
+	"machakann/vim-highlightedyank", -- highlight when use y
+	"bogado/file-line", -- in to enable vim to open the file in the spefic line
+	"eugen0329/vim-esearch", -- search
+	"RRethy/nvim-treesitter-endwise", -- auto add end in functions
+	"mhinz/vim-startify", -- fancy initial screen to vim
+	"tpope/vim-eunuch", -- Easialy rename files using :Rename filename newfilename
 
-  -- Todo highligth and list
-  {
-    'folke/todo-comments.nvim',
-    config = function()
-      require("todo-comments").setup()
-    end
-  },
+	-- Todo highligth and list
+	{
+		"folke/todo-comments.nvim",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
 
-  -- highlight same words
-  {
-    "echasnovski/mini.cursorword",
-    config = function()
-      require('mini.cursorword').setup({ delay = 300 })
-    end
-  },
+	-- highlight same words
+	{
+		"echasnovski/mini.cursorword",
+		config = function()
+			require("mini.cursorword").setup({ delay = 300 })
+		end,
+	},
 
-  -- see yanked values in telescope
-  {
-    "AckslD/nvim-neoclip.lua",
-    dependencies = { 'nvim-telescope/telescope.nvim' },
-    config = function()
-      require('neoclip').setup()
-    end,
-  },
+	-- see yanked values in telescope
+	{
+		"AckslD/nvim-neoclip.lua",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+		config = function()
+			require("neoclip").setup()
+		end,
+	},
 
-  -- http request directly on vim
-  { "rest-nvim/rest.nvim" },
+	-- http request directly on vim
+	-- {
+	-- 	"rest-nvim/rest.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		opts = function(_, opts)
+	-- 			opts.ensure_installed = opts.ensure_installed or {}
+	-- 			table.insert(opts.ensure_installed, "http")
+	-- 		end,
+	-- 	},
+	-- },
 
-  -- splitting/joining blocks
-  {
-    'Wansmer/treesj',
-    keys = { '<space>m' }, -- toggle the joining/splitting
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    config = function()
-      require('treesj').setup({})
-    end,
-  }
+	-- splitting/joining blocks
+	{
+		"Wansmer/treesj",
+		keys = { "<space>m" }, -- toggle the joining/splitting
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesj").setup({})
+		end,
+	},
 }
-
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,35 +1,46 @@
 local u = require("utils")
 local nnoremap = u.nnoremap
 
-local LSP_CONFIG = { "neovim/nvim-lspconfig" }
 local MASON = { "williamboman/mason.nvim" }
 local MASON_LSP_CONFIG = { "williamboman/mason-lspconfig.nvim" }
 local HELM_LS = {
-    "qvalentin/helm-ls.nvim",
-    ft = "helm",
-    config = {
-        conceal_templates = {
-            -- enable the replacement of templates with virtual text of their current values
-            enabled = false,
-        },
-        indent_hints = {
-            -- enable hints for indent and nindent functions
-            enabled = true,
-            -- show the hints only for the line the cursor is on
-            only_for_current_line = true,
-        },
+  "qvalentin/helm-ls.nvim",
+  ft = "helm",
+  config = {
+    conceal_templates = {
+      -- enable the replacement of templates with virtual text of their current values
+      enabled = false,
     },
+    indent_hints = {
+      -- enable hints for indent and nindent functions
+      enabled = true,
+      -- show the hints only for the line the cursor is on
+      only_for_current_line = true,
+    },
+  },
 }
 
 MASON.config = function()
-    require("mason").setup()
+  require("mason").setup({
+    ensure_installed = {
+      "lua-language-server",
+      "docker-compose-language-service",
+      "dockerfile-language-server",
+      "eslint-lsp",
+      "rubocop",
+      "ruby-lsp",
+      "sqlls",
+      "typescript-language-server",
+      "yaml-language-server",
+    },
+  })
 end
 
 -- show float with error
 nnoremap("Z", vim.diagnostic.open_float, { desc = "Show the diagnostic in a float" })
 nnoremap("S", function()
-    local new_config = not vim.diagnostic.config().virtual_lines
-    vim.diagnostic.config({ virtual_lines = new_config })
+  local new_config = not vim.diagnostic.config().virtual_lines
+  vim.diagnostic.config({ virtual_lines = new_config })
 end, { desc = "Toggle diagnostic virtual_lines" })
 
 -------------------------
@@ -37,98 +48,92 @@ end, { desc = "Toggle diagnostic virtual_lines" })
 -------------------------
 
 vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            runtime = { version = "LuaJIT" },
-            diagnostics = { globals = { "vim" } },
-            workspace = {
-                checkThirdParty = false,
-                library = {
-                    vim.api.nvim_get_runtime_file("", true),
-                },
-            },
-            telemetry = { enable = false },
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.api.nvim_get_runtime_file("", true),
         },
+      },
+      telemetry = { enable = false },
     },
+  },
 })
 
 vim.lsp.config("eslint", {
-    on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-        })
-    end,
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
 })
 
 vim.lsp.config("helm_ls", {
-    settings = {
-        ["helm-ls"] = {
-            yamlls = {
-                path = "yaml-language-server",
-            },
-        },
+  settings = {
+    ["helm-ls"] = {
+      yamlls = {
+        path = "yaml-language-server",
+      },
     },
+  },
 })
 
-----------------------
-----------------------
-----------------------
-
+-- mason-lspconfig auto-enables all Mason-installed servers via vim.lsp.enable()
 MASON_LSP_CONFIG.config = function()
-    require("mason-lspconfig").setup({
-        ensure_installed = {
-            "lua_ls",
-            "docker_compose_language_service",
-            "dockerls",
-            "eslint",
-            "rubocop",
-            "ruby_lsp",
-            "sqlls",
-            "ts_ls",
-            "yamlls",
-        },
-    })
+  require("mason-lspconfig").setup({
+    ensure_installed = {
+      "lua_ls",
+      "docker_compose_language_service",
+      "dockerls",
+      "eslint",
+      "rubocop",
+      "ruby_lsp",
+      "sqlls",
+      "ts_ls",
+      "yamlls",
+    },
+  })
 end
 
 local ELIXIR_TOOLS = {
-    "elixir-tools/elixir-tools.nvim",
-    version = "*",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local elixir = require("elixir")
-        local elixirls = require("elixir.elixirls")
+  "elixir-tools/elixir-tools.nvim",
+  version = "*",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local elixir = require("elixir")
+    local elixirls = require("elixir.elixirls")
 
-        elixir.setup({
-            nextls = { enable = true },
-            elixirls = {
-                enable = true,
-                settings = elixirls.settings({
-                    dialyzerEnabled = false,
-                    enableTestLenses = false,
-                }),
-                on_attach = function(client, bufnr)
-                    vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-                    vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-                    vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-                end,
-            },
-            projectionist = {
-                enable = true,
-            },
-        })
-    end,
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
+    elixir.setup({
+      nextls = { enable = true },
+      elixirls = {
+        enable = true,
+        settings = elixirls.settings({
+          dialyzerEnabled = false,
+          enableTestLenses = false,
+        }),
+        on_attach = function(client, bufnr)
+          vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+        end,
+      },
+      projectionist = {
+        enable = true,
+      },
+    })
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
 }
 
-vim.lsp.enable("postgres_lsp")
-
 return {
-    LSP_CONFIG,
-    MASON,
-    MASON_LSP_CONFIG,
-    ELIXIR_TOOLS,
-    HELM_LS,
+  MASON,
+  MASON_LSP_CONFIG,
+  -- ELIXIR_TOOLS,
+  HELM_LS,
 }
